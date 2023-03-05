8bb477da738a3c109b05484b505d278786cdc7c1
Maria luz Opazo <91832154+marialuzopazo@users.noreply.github.com>
2023-03-05 13:03:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var nombre="maria luz"
+var apellido="opazo"
+var operacion=nombre"maria luz"+apellido"opazo"
+console.log(operacion) 
+
+var numero1="1"
+var numero2="2"
+var suma=numero1"1"+numero2"2";
+console.log(suma);
